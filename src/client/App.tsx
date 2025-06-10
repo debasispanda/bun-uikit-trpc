98bbdc261b014f455885469ed911abbfd25bb167
Debasis Panda <debasispanda@MacBook-Air.local>
2025-06-10 06:25:50 +0530

    initial client setup

---
+++ src/client/App.tsx
@@ -0,0 +1,12 @@
+import { HvProvider } from '@hitachivantara/uikit-react-core'
+import { Note } from './components/Note/Note'
+
+function App() {
+  return (
+    <HvProvider>
+      <Note />
+    </HvProvider>
+  )
+}
+
+export default App
---
+++ src/client/components/Note/Note.tsx
@@ -0,0 +1,11 @@
+import { HvButton } from "@hitachivantara/uikit-react-core";
+
+export const Note = () => {
+  return (
+    <HvButton
+      variant="primary"
+    >
+      Note
+    </HvButton>
+  );
+}
